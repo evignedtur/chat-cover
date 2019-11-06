4d41f91c0e7c45b273d734aca71f74db8d47f433
evignedtur <evignedtur@protonmail.com>
2019-11-06 13:24:45 +0100

    Updates

---
+++ precache-manifest.ee881d93692236bd7d3bbef64914862e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6fdef2c76314a6b4cc1ab3e2a3a26f",
+    "url": "/chat-cover/index.html"
+  },
+  {
+    "revision": "f1f55083d8c9f9c250d1",
+    "url": "/chat-cover/static/css/main.3fc84a98.chunk.css"
+  },
+  {
+    "revision": "52d84532b5233a7c3e59",
+    "url": "/chat-cover/static/js/2.ae444982.chunk.js"
+  },
+  {
+    "revision": "f1f55083d8c9f9c250d1",
+    "url": "/chat-cover/static/js/main.7703a8e2.chunk.js"
+  },
+  {
+    "revision": "8a6ab46ce3ec21435355",
+    "url": "/chat-cover/static/js/runtime-main.6af51106.js"
+  }
+]);
